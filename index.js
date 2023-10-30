aff40318f28df56811399d9b1c977ab48b6c9e40
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-10-29 21:13:50 -0600

    still no idea works on codepen but not here

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict"
-    
+    function setItem(){
      // var data = localStorage.getItem("userName");
       const charText = document.querySelector('div');
       charText.innerHTML =
           `   <div class="Username">Username: <span class="User-name"> jose </span>`;
+    }
