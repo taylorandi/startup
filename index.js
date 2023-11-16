ba702bb8668378a85b571e8d0015a3762f6698e8
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-11-16 15:26:11 -0700

    got express installed now to add the index.js and make it work

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-
-
-class user{
-  constructor(){
-    const user = getUser();
-    setUser(user);
-  }
-
-getUser() {
-  return localStorage.getItem("userName");
-}
-     setUser(user){
-      const charText = document.document.querySelector('span');
-      charText.innerHTML =
-          `<span class="User-name"> ${user} </span>`;
-     }
-    }
+const express = require('express');
+const app = express();
