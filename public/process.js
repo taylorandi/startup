913f8be2f2260fa827f83c46ad6e59bba96e3a45
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-12-02 13:46:29 -0700

    adding some more elements to further implement the code

---
+++ public/process.js
@@ -0,0 +1,26 @@
+async function displayId() {
+    orderNum = [];
+    try {
+        // Get the latest high scores from the service
+        const response = await fetch('/api/orderId');
+        orderNum = await response.json();
+
+
+    } catch {
+        // If there was an error then just use the last saved scores
+        const scoresText = localStorage.getItem('orderId');
+        if (scoresText) {
+            orderNum = JSON.parse(scoresText);
+        }
+    }
+
+    displayOrder(orderNum);
+}
+
+function displayOrder(orderNum) {
+    const tableBodyEl = document.querySelector('#order');
+    tableBodyEl.appendChild(orderNum);
+}
+
+
+displayId();
