9d0ff3afdecf983e823198d6b967a7e1642040c7
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-12-02 21:45:27 -0700

    still a lot of work to figure out how i broke it

--- public/quote.js
+++ public/quote.js
@@ -5,25 +5,13 @@ function passValues() {
   const selectedFile = document.getElementById("file").files[0];
   const qty = document.querySelector("#qty");
   const type = document.querySelector("#tysel");
-  //fetching the number of the last order so the new one can be compiled
-  async function loadId(){
-    try {
-      // Get the latest high scores from the service
-      const response = await fetch('/api/orderId');
-      orderId = await response.json();
-
-
-  } catch {
-      // If there was an error then just use the last saved scores
-      const scoresText = localStorage.getItem('orderId');
-      if (scoresText) {
-          orderId = JSON.parse(scoresText);
-      }
-  }
-  }
 
   //updating to the next order number, will require database to better function. using place holder of one for now
-  orderId.orderId = 1;
+  const orderId = 1;
+  setId(orderId);
+  window.location.href = "process.html";
+}
+
   async function setId(orderId){
     const userName = localStorage.getItem("userName");
     const newOrder = {name: userName, orderId: orderId, date: date}
@@ -40,5 +28,4 @@ function passValues() {
     } catch {
     }
   }
-  window.location.href = "process.html";
-}
+  
