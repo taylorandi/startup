1be90b15b6b90a984d6391e0f7546a3b3f8d9222
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-12-07 16:11:42 -0700

    found one of the major bugs, also was deploying to the simon not startup link

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+const config = require('./dbconfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
