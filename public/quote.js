cdaeb2e14b5115f5699f3bbd49e70c62198ec5de
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-12-08 11:11:52 -0700

    i can write to the database just not reading very well

--- public/quote.js
+++ public/quote.js
@@ -1,10 +1,10 @@
 async function passValues() {
   const stl = document.querySelector("#stl:checked");
   const jpeg = document.querySelector("#jpeg:checked");
-  document.querySelector("#cad:checked");
+  const cad = document.querySelector("#cad:checked");
   const selectedFile = document.getElementById("file").files[0];
   const qty = document.querySelector("#qty");
-  const type = document.querySelector("#tysel");
+  const tysel = document.querySelector("#tysel");
 
   //updating to the next order number, will require database to better function. using place holder of one for now
   const orderId = 1;
@@ -15,7 +15,7 @@ async function passValues() {
 async function setId(ordernum) {
   const userName = localStorage.getItem("userName");
   const date = new Date().toLocaleDateString();
-  const newOrder = { name: userName, orderId: ordernum, date: date, document: selectedFile.value, qty: qty.value, type: type.value };
+  const newOrder = { name: userName, orderId: ordernum, date: date, qty: qty.value, type: tysel.value };
   try {
     const response = await fetch('/api/orderId', {
       method: 'POST',
