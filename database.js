eabe2ed090c789d9e3713ca8ba1363c609513700
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-12-07 18:40:10 -0700

    still working a few changes

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const config = require('./dbconfig.json');
+const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
