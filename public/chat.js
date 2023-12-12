51c569d094fd6ccc8d978583cfbde0a5c9cf35e3
Taylor Anderton <taylorj.anderton@gmail.comm>
2023-12-12 14:00:17 -0700

    simple websocket added no time to make it better

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,25 @@
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+// Display that we have opened the webSocket
+socket.onopen = (event) => {
+  appendMsg('system', 'websocket', 'connected');
+};
+
+// Display messages we receive from our friends
+socket.onmessage = async (event) => {
+  const text = await event.data.text();
+  const chat = JSON.parse(text);
+  appendMsg('friend', chat.name, chat.msg);
+};
+
+// If the webSocket is closed then disable the interface
+socket.onclose = (event) => {
+  appendMsg('system', 'websocket', 'disconnected');
+  document.querySelector('#name-controls').disabled = true;
+  document.querySelector('#chat-controls').disabled = true;
+};
+
 const chatControls = document.querySelector('#chat-controls');
 const myName = localStorage.getItem("userName");
 chatControls.disabled = myName.value === '';
@@ -19,7 +41,7 @@ function sendMessage() {
     const msg = msgEl.value;
     if (!!msg) {
       appendMsg('me', 'me', msg);
-      const name = chatFunction;
+      const name = myName;
       socket.send(`{"name":"${name}", "msg":"${msg}"}`);
       msgEl.value = '';
     }
